8fb34056a331bfd7e61b5c3939d3aa33cb03a060
Wolfgang Marcos <3627736+brwolfgang@users.noreply.github.com>
2019-05-06 21:41:23 -0400

    Inicia migração da extensão existente para Vue

--- README.md
+++ README.md
@@ -4,34 +4,38 @@
 [<img align="right" src="https://img.shields.io/github/stars/mozillabrasil/sumo_live_helper.svg">](https://img.shields.io/github/stars/mozillabrasil/sumo_live_helper.svg)
 [<img align="right" src="https://img.shields.io/github/release/mozillabrasil/sumo_live_helper.svg">](https://img.shields.io/github/release/mozillabrasil/sumo_live_helper.svg)
 
-![sumo_live_helper](src/res/icons/icon.png)
+![sumo_live_helper](src/icons/icon.png)
+
 # SUMO Live Helper
 
 Working to add functionalities ...
 
 ## Requirements
 
-* Firefox 58.0 or later
+- Firefox 58.0 or later
 
 ## How to test this extension
 
-1. Clone this repository git clone ```https://github.com/mozillabrasil/sumo_live_helper.git```
+1. Clone this repository git clone `https://github.com/mozillabrasil/sumo_live_helper.git`
 
-2. In the Firefox address bar type ```about:debugging```
+2. In the Firefox address bar type `about:debugging`
 
-3. Click ```Load Temporary Add-on```, a dialog window opens.
+3. Click `Load Temporary Add-on`, a dialog window opens.
 
-4. In the cloned repository, select the ```manifest.json``` file. The webextension icon will appear in your toolbar.
+4. In the cloned repository, select the `manifest.json` file. The webextension icon will appear in your toolbar.
 
 ## How Contribute
 
 Have a suggestion, doubt or encountered an error? Please open a new [issue](https://github.com/mozillabrasil/sumo_live_helper/issues).
 
 ## Learn More
+
 To learn more about webextensions development, see this [Mozilla developer Network documentation](https://developer.mozilla.org/en-US/Add-ons/WebExtensions).
 
 ### License
+
 This repository is available under the [Mozilla Public License 2.0](https://github.com/mozillabrasil/sumo_live_helper/blob/master/LICENSE)
 
 ### Credits
+
 Thank you [philipp-sumo](https://github.com/philipp-sumo/) for your helpfull tips and time to revive this project.
--- src/js/preferences.js
+++ src/js/preferences.js
@@ -1,47 +1,46 @@
 function salvarPreferencias(e) {
-    e.preventDefault();
-    let preferencias = {
-        buscarQuestoesAtivamente: document.getElementById("buscarQuestoesAtivamente").checked,
-        frequenciaBuscaQuestoes: document.getElementById("frequenciaBuscaQuestoes").value
-    };
+  e.preventDefault();
+  let preferencias = {
+    buscarQuestoesAtivamente: document.getElementById('buscarQuestoesAtivamente').checked,
+    frequenciaBuscaQuestoes: document.getElementById('frequenciaBuscaQuestoes').value,
+  };
 
-    browser.storage.sync.set(preferencias);
+  browser.storage.sync.set(preferencias);
 
-    bootbox.alert({
-        message: "Preferências salvas",
-        size: "small"
-    });
-    
-    console.log("Preferências salvas");
+  bootbox.alert({
+    message: 'Preferências salvas',
+    size: 'small',
+  });
+
+  console.log('Preferências salvas');
 }
 
 function carregarPreferencias() {
-    function definirBuscarQuestoesAtivamente(dados) {
-        console.log(dados);
-        if (dados.buscarQuestoesAtivamente == true) {
-            document.getElementById("buscarQuestoesAtivamente").checked = true
-        }
+  function definirBuscarQuestoesAtivamente(dados) {
+    console.log(dados);
+    if (dados.buscarQuestoesAtivamente == true) {
+      document.getElementById('buscarQuestoesAtivamente').checked = true;
     }
+  }
 
-    function definirFrequenciaBuscaQuestoes(dados) {
-        if (dados.frequenciaBuscaQuestoes) {
-            document.getElementById("frequenciaBuscaQuestoes").value = dados.frequenciaBuscaQuestoes;
-        }
+  function definirFrequenciaBuscaQuestoes(dados) {
+    if (dados.frequenciaBuscaQuestoes) {
+      document.getElementById('frequenciaBuscaQuestoes').value = dados.frequenciaBuscaQuestoes;
     }
+  }
 
-    function onError(error) {
-        console.log(`Error: ${error}`);
-    }
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
 
-    var promiseBuscarQuestoesAtivamente = browser.storage.sync.get("buscarQuestoesAtivamente");
-    var promiseFrequenciaBuscaQuestoes = browser.storage.sync.get("frequenciaBuscaQuestoes");
+  var promiseBuscarQuestoesAtivamente = browser.storage.sync.get('buscarQuestoesAtivamente');
+  var promiseFrequenciaBuscaQuestoes = browser.storage.sync.get('frequenciaBuscaQuestoes');
 
-    promiseBuscarQuestoesAtivamente.then(definirBuscarQuestoesAtivamente, onError);
-    promiseFrequenciaBuscaQuestoes.then(definirFrequenciaBuscaQuestoes, onError);
-    
-    
-    console.log("Preferências carregadas");
+  promiseBuscarQuestoesAtivamente.then(definirBuscarQuestoesAtivamente, onError);
+  promiseFrequenciaBuscaQuestoes.then(definirFrequenciaBuscaQuestoes, onError);
+
+  console.log('Preferências carregadas');
 }
 
-document.addEventListener("DOMContentLoaded", carregarPreferencias);
-document.querySelector("form").addEventListener("submit", salvarPreferencias);
+document.addEventListener('DOMContentLoaded', carregarPreferencias);
+document.querySelector('form').addEventListener('submit', salvarPreferencias);
